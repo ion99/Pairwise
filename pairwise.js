a9f54c24d31762b7a064802a5c04ed96b72d9e86
Ion Gorincioi <gorincioii@yahoo.com>
2016-11-02 10:45:16 -0700

    ES5 => ES6, var => let and reg fun(){} => arrow fun()

--- pairwise.js
+++ pairwise.js
@@ -2,13 +2,13 @@ function pairwise(arr, arg) {
   if (arr.length === 0){
     return 0;
   }
-  var result = [];  
-  for (var i = 0; i<arr.length; i++){
-    for (var j = i+1; j<arr.length; j++){
+  let result = [];  
+  for (let i = 0; i < arr.length; i++){
+    for (let j = i+1; j < arr.length; j++){
       if (arr[i] + arr[j] === arg && result.indexOf(i) === -1 && result.indexOf(j) === -1){
       result.push(i, j);
       }
     }
   }
-  return result.reduce(function(a, b){ return a + b; });
+  return result.reduce((a, b) => a + b, 0);
 }
